9b9c49585925d883e2a8d470b11f1f5d04a4401e
Brian Nguyen <BrianCTNguyen@knights.ucf.edu>
2021-06-06 22:03:33 -0400

    initial solution to problem 7

--- src/main/java/rectangle/app.java
+++ src/main/java/rectangle/app.java
@@ -19,6 +19,7 @@ You entered dimensions of 15 feet by 20 feet.
 The area is
 300 square feet
 27.871 square meters
+
 The formula for this conversion is m2 = f2 × 0.09290304
 
 Constraints
@@ -38,6 +39,21 @@ public class app {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.print("What is the length of the room in feet? ");
+        double length = in.nextDouble();
+
+        System.out.print("What is the width of the room in feet? ");
+        double width = in.nextDouble();
+
+        System.out.println("You entered dimensions of " + length + " by " + width + " feet");
+
+        double area = length*width;
+        double conversionFactor = area*0.09290304;
+
+
+        System.out.println("The area is");
+        System.out.println(area+ " square feet");
+        System.out.println(conversionFactor+ " square meters");
 
     }
 }
